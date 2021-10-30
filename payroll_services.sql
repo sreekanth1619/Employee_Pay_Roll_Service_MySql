a9a5db886767348921297444ca3d000389b0c803
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-30 15:13:38 +0530

    Added: Ability to create Employee payroll table

--- payroll_services.sql
+++ payroll_services.sql
@@ -5,3 +5,12 @@ show databases;
 use payroll_services;
 select database();
 
+-- UC2
+
+create table employee_payroll
+( id int not null auto_increment ,
+name varchar(50) not null,
+salary Double not null,
+startDate Date not null,
+primary key(id)
+);
