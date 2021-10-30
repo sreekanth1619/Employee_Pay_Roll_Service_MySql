d69def9554093f1a1e3b356036bca7d9c9a87f21
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-30 15:21:32 +0530

    Added: Ability to create Employee Payroll data in payroll

--- payroll_services.sql
+++ payroll_services.sql
@@ -14,3 +14,20 @@ salary Double not null,
 startDate Date not null,
 primary key(id)
 );
+
+desc employee_payroll;
+select * from employee_payroll;
+
+-- UC3
+
+insert into employee_payroll (name,salary,startDate)
+values('Prashanth', 38000, '2021-01-10');
+
+insert into employee_payroll (name,salary,startDate)
+values('Navin', 30000, '2021-04-10');
+
+insert into employee_payroll (name,salary,startDate)
+values('Utkarsh', 45000, '2020-02-20');
+
+insert into employee_payroll (name,salary,startDate)
+values('Ravi', 25000, '2020-10-10');
